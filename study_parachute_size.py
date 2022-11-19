8dab212fbc32e176e3edcbe6aa2f97a3d4c56ee2
EvanWarrick <114360240+EvanWarrick@users.noreply.github.com>
2022-11-18 23:51:12 -0600

    Update study_parachute_size.py

--- study_parachute_size.py
+++ study_parachute_size.py
@@ -25,20 +25,20 @@ for i in diam:
     edl_system['velocity'] = -578     # [m/s] initial velocity
     edl_system['parachute']['deployed'] = True   # our parachute is open
     edl_system['parachute']['ejected'] = False   # and still attached
-    edl_system['heat_shield']['ejected'] = False
-    edl_system['sky_crane']['on'] = False
-    edl_system['rocket']['on'] = False
-    edl_system['speed_control']['on'] = False
-    edl_system['position_control']['on'] = False
+    edl_system['heat_shield']['ejected'] = False   #reattach the heat  shield
+    edl_system['sky_crane']['on'] = False   #disable sky crane
+    edl_system['rocket']['on'] = False          #disable rockets
+    edl_system['speed_control']['on'] = False       #disable speed control
+    edl_system['position_control']['on'] = False    #disable position control
     edl_system['rover']['on_ground'] = False # the rover has not yet landed
-    edl_system['parachute']['diameter'] = float(i)
+    edl_system['parachute']['diameter'] = float(i) #change the value of parachute diameter
     print('Simulation with parachute of diameter of',i,'meters:\n')
     print('-'*54+'\n')
-    [t, Y, edl_system] = simulate_edl(edl_system, mars, mission_events, tmax, True)
+    [t, Y, edl_system] = simulate_edl(edl_system, mars, mission_events, tmax, True)     #retrieve simulation data
     param.append([t,Y,edl_system])
-    sim_time.append(max(t))
-    rover_speed.append(Y[0,-1])
-    if Y[0,-1] <= 1 and edl_system['altitude'] >= 4.5:
+    sim_time.append(max(t))     #add the simulation time to sim_time
+    rover_speed.append(Y[0,-1])     #add the final rover speed to rover_speed
+    if Y[0,-1] <= 1 and edl_system['altitude'] >= 4.5:  #if the rover meets the critical conditions, the simulation succeeded
         success.append(1)
     else:
         success.append(0)
